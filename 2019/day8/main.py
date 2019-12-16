41e60ec26e57860646d245378fdbe2fbaeebed43
Julien Gaston <gaston@codelutin.com>
2019-12-16 16:38:47 +0100

    Day 8 part two

--- 2019/day8/main.py
+++ 2019/day8/main.py
@@ -1,10 +1,10 @@
 def layer_generator(data, width, height):
-    image = [int(value) for value in data]
-    image_size = len(image)
+    image_data = [int(value) for value in data]
+    image_size = len(image_data)
     layer_size = int(width * height)
     layer_count = int(image_size / layer_size)
     for index in range(0, layer_count):
-        layer = image[index*layer_size:(index+1)*layer_size]
+        layer = image_data[index*layer_size:(index+1)*layer_size]
         yield layer
 
 def count_digit(layer, digit):
@@ -13,15 +13,28 @@ def count_digit(layer, digit):
 def count_zero(layer):
     return count_digit(layer, 0)
 
-def part_one(data, width, height):
-    layers = layer_generator(data, width, height)
-    layer = min(layers, key=lambda l: count_zero(l))
-    ones_count = count_digit(layer, 1)
-    twos_count = count_digit(layer, 2)
-    print(ones_count * twos_count)
+def get_pixel_value(layers, width, x, y):
+    for layer in layers:
+        pixel = layer[x + width*y]
+        if pixel == 0 or pixel == 1:
+            return pixel
+    return 2
+
+def part_two(data, width, height):
+    layers = list(layer_generator(data, width, height))
+    for y in range(0, height):
+        for x in range(0, width):
+            p = get_pixel_value(layers, width, x, y)
+            if p == 0:
+                print("_", end="")
+            elif p == 1:
+                print("X", end="")
+            else:
+                print(" ", end="")
+        print("")
 
 if __name__ == "__main__":
-    #part_one("122456789012", 3, 2)
+    #part_two("0222112222120000", 2, 2)
     with open("./input.txt") as input:
         data = input.readline()
-        part_one(data, 25, 6)
+        part_two(data, 25, 6)
